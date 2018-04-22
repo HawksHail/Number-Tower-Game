648ba3b5759f4a65f2900a2bfaf2c2280da8c44d
HawksHail <zionman@verizon.net>
2018-04-22 15:47:17 -0500

    Semi working main

--- main.asm
+++ main.asm
@@ -4,6 +4,8 @@
 .data
 array: .space 112 	# pyramid array
 invalidStr: .asciiz "\nThe input was invalid. "
+verifyStr: .asciiz "\nThe input could not be checked. Do you still want to enter it? (y or n) "
+notConfirmed: .asciiz "\nThe input was not entered. "
 .globl main
 .text
 
@@ -12,29 +14,29 @@ main:
 la $a0, array
 jal load
 
-move $s0, $v0
+move $s0, $v0	#save pyramid address
 
-move $a0, $s0
-jal printPyr
+move $a0, $s0	#move pyramid address to a0 for print
+start:
+jal printPyr	# print pyr
 
-jal getInput
+jal getInput	# get user input
 
-move $a0, $s0
+move $a0, $s0	#save user input
 move $a1, $v0
 move $a2, $v1
-jal verifyInput
+jal verifyInput	# check input
 
-bne $v0, 0, invalid
-sll $t9, $a1, 2
-add $t9, $t9, $a0
-sw $a2, 0($t9)
+beq $v0, 1, invalid		# if input is invalid print error
+beq $v0, 2, invalid		# if input is invalid print error
 
-move $a0, $s0
-jal printPyr
+beq $v0, 3, verify		# if input is uncheckable, confirm print error
 
-
-li $v0, 10 		# exit
-syscall
+cont:
+sll $t5, $a1, 2			# update pyramid to include 
+add $t5, $t5, $s0
+sw $a2, 0($t5)
+j checkDone
 
 
 
@@ -43,4 +45,112 @@ li $v0, 4		# print string, prompt
 la $a0, invalidStr
 syscall
 
+verify:
+li $v0, 4		# print string, prompt
+la $a0, verifyStr
+syscall
 
+li $v0, 12		# read char, label
+syscall
+
+beq $v0, 121, confirmed		# if y				
+li $v0, 4		# print string, prompt
+la $a0, notConfirmed
+syscall
+j start
+
+confirmed:
+j cont		#continue
+
+
+checkDone:
+lw $t9, 0($s0)
+beq $t9, 0, start
+
+lw $t9, 4($s0)
+beq $t9, 0, start
+
+lw $t9, 8($s0)
+beq $t9, 0, start
+
+lw $t9, 12($s0)
+beq $t9, 0, start
+
+lw $t9, 16($s0)
+beq $t9, 0, start
+
+lw $t9, 20($s0)
+beq $t9, 0, start
+
+lw $t9, 24($s0)
+beq $t9, 0, start
+
+lw $t9, 28($s0)
+beq $t9, 0, start
+
+lw $t9, 32($s0)
+beq $t9, 0, start
+
+lw $t9, 36($s0)
+beq $t9, 0, start
+
+lw $t9, 40($s0)
+beq $t9, 0, start
+
+lw $t9, 44($s0)
+beq $t9, 0, start
+
+lw $t9, 48($s0)
+beq $t9, 0, start
+
+lw $t9, 52($s0)
+beq $t9, 0, start
+
+lw $t9, 56($s0)
+beq $t9, 0, start
+
+lw $t9, 60($s0)
+beq $t9, 0, start
+
+lw $t9, 64($s0)
+beq $t9, 0, start
+
+lw $t9, 68($s0)
+beq $t9, 0, start
+
+lw $t9, 72($s0)
+beq $t9, 0, start
+
+lw $t9, 76($s0)
+beq $t9, 0, start
+
+lw $t9, 80($s0)
+beq $t9, 0, start
+
+lw $t9, 84($s0)
+beq $t9, 0, start
+
+lw $t9, 88($s0)
+beq $t9, 0, start
+
+lw $t9, 92($s0)
+beq $t9, 0, start
+
+lw $t9, 96($s0)
+beq $t9, 0, start
+
+lw $t9, 100($s0)
+beq $t9, 0, start
+
+lw $t9, 104($s0)
+beq $t9, 0, start
+
+lw $t9, 108($s0)
+beq $t9, 0, start
+
+# Done
+jal printPyr	# print pyr
+jal midi
+
+li $v0, 10 		# exit
+syscall
